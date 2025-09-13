730e66d33b56813f2097a30762a9dfcb221f88aa
Gustavo Canuto <gcanuto.dev@gmail.com>
2025-09-13 17:12:15 -0300

    feat: iniciado projeto backend pet shop service

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\TesteController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/teste', [TesteController::class, 'index']);
